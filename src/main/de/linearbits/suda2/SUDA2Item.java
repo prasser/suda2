91426ad12250f531a8d2a7306d3dc524758bd306
RaffaelBild <raffael.bild@gmx.de>
2017-10-18 10:54:02 +0200

    Fix timing of methods

--- src/main/de/linearbits/suda2/SUDA2Item.java
+++ src/main/de/linearbits/suda2/SUDA2Item.java
@@ -128,7 +128,7 @@ public class SUDA2Item extends Timeable {
      */
     public SUDA2Item getProjection(SUDA2IntSet otherRows) {
         
-        startTiming();
+        long startTime = System.nanoTime();
 
         // Smaller set is rows1
         int size1 = this.rows.size();
@@ -139,7 +139,7 @@ public class SUDA2Item extends Timeable {
         // Intersect
         SUDA2IntSet rows = rows1.intersectWith(rows2);
         
-        endTiming(METHOD_PROJECTION);
+        endTiming(METHOD_PROJECTION, startTime);
 
         // Return
         return rows.size() == 0 ? null : new SUDA2Item(this.column, this.value, this.id, rows);
